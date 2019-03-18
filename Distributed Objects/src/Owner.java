c7903f0a354e67483290f328f981c27e178e23ab
CarolineZhou <c_z.y@live.cn>
2019-03-18 13:40:11 -0600

    Selection of objects created

---
+++ Distributed Objects/src/Owner.java
@@ -0,0 +1,16 @@
+
+public class Owner {
+	private int age;
+	private int birthYear;
+	
+	public void setAge(int a)
+	{
+		age = a;
+	}
+	
+	public void setBirthYear(int year)
+	{
+		birthYear = year;
+	}
+	
+}
