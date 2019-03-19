d564ccd75746fb36e78b034f90880523eb3a8796
CarolineZhou <c_z.y@live.cn>
2019-03-18 22:55:35 -0600

    Working on ObjectCreator.java

--- Distributed Objects/src/Owner.java
+++ Distributed Objects/src/Owner.java
@@ -1,16 +1,11 @@
 
 public class Owner {
 	private int age;
-	private int birthYear;
 	
 	public void setAge(int a)
 	{
 		age = a;
 	}
 	
-	public void setBirthYear(int year)
-	{
-		birthYear = year;
-	}
 	
 }
